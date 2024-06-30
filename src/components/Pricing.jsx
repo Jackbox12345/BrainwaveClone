0753d4f795ca4c184c166e2d8c2f9a8a4268abd4
Christian F. Lapitan <cmlapitan@panasiaticsolutions.net>
2024-06-30 12:30:46 +0800

    Services

---
+++ src/components/Pricing.jsx
@@ -0,0 +1,14 @@
+import Heading from "./Heading";
+import Section from "./Section";
+
+const Pricing = () => {
+  return (
+    <Section crosses>
+      <div className="container">
+        <Heading title="Pricing" />
+      </div>
+    </Section>
+  );
+};
+
+export default Pricing;
